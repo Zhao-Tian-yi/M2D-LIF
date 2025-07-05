19015a31dadf71b6dc33a2177b8087d58c406931
Maerceci-27 <22373397@buaa.edu.cn>
2025-07-05 19:12:18 +0800

    Update val_obb.py

--- val_obb.py
+++ val_obb.py
@@ -4,11 +4,11 @@ from datetime import datetime
 
 if __name__ == '__main__':
 
-    data = r"/home/zhaotianyi/M2D-LIF/M2D-LIF_open_source/data/DroneVehicle.yaml"
+    data = r"./data/DroneVehicle.yaml"
 
     from ultralytics.models.yolo.obb import OBBValidator
 
-    args = dict(model="/home/zhaotianyi/M2D-LIF/M2D-LIF_open_source/checkpoint/multimodal/DroneVehicle.pt", data=data,
+    args = dict(model="./checkpoint/multimodal/DroneVehicle.pt", data=data,
                 device=5,
                 imgsz=640, batch=1, save=True)
     validator = OBBValidator(args=args)
