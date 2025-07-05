8b77a2a69d2453889bc669412ff2924feca343b4
Maerceci-27 <22373397@buaa.edu.cn>
2025-07-05 14:21:15 +0800

    Update train_dist_obb.py

--- train_dist_obb.py
+++ train_dist_obb.py
@@ -11,22 +11,6 @@ from ultralytics.utils import DEFAULT_CFG
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    """
-    1.修改trainer.py中的特征通道数
-    2.model_s为要训练的模型
-    3.更改写在类Distillation_loss的 channels_s和channels_t，
-        将通道数改成自己模型的通道数, 路径在
-        ultralytics/engine/trainer.py
-    4.如果不需要蒸馏, 则直接把Distillation设置成None即可
-    5.多模态蒸馏, 把Distillation设置为"MultiDistillation",
-      并设置Teacher_Model_RGB和Teacher_Model_IR
-      单模态蒸馏, 把Distillation设置为"SingleDistillation",
-      并设置Teacher_Model
-    Attributes:
-        Distillation: the distillation model
-        loss_type: MGD, CWD, PKD
-        amp: Automatic Mixed Precision
-    """
 
     # DroneVehicle TEACHER
     _, model_t_rgb = attempt_load_one_weight(
@@ -39,7 +23,7 @@ if __name__ == '__main__':
 
     args = dict(
         model=r"/home/zhaotianyi/M2D-LIF/M2D-LIF_open_source/model_yaml_obb/yolov8_LIF_obb.yaml",
-        data=r"/home/zhaotianyi/M2D-LIF/M2D-LIF_open_source/data/DroneVehicle.yaml",  # DroneVehicle
+        data=r"/home/zhaotianyi/M2D-LIF/M2D-LIF_open_source/data/DroneVehicle.yaml", 
         Distillation="MultiDistillation",
         distill_weight=0.8,
         Teacher_Model_RGB=model_t_rgb["model"],
@@ -47,12 +31,11 @@ if __name__ == '__main__':
         loss_type="CWD",
         amp=False,
         imgsz=640,
-        epochs=200,
+        epochs=100,
         batch=8,
         device=5,
         lr0=0.001,
         online=False,
-        augment=True,
         workers=4
     )
 
