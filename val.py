793cdffe89aba8c2c4fe8a347eec2df1f45c3ecd
Maerceci-27 <22373397@buaa.edu.cn>
2025-07-05 19:07:49 +0800

    Update val.py

--- val.py
+++ val.py
@@ -3,14 +3,14 @@ from ultralytics.utils import DEFAULT_CFG
 from datetime import datetime
 
 if __name__ == '__main__':
-    model = YOLO(r"/home/zhaotianyi/M2D-LIF/runs/FLIR/speed_test/weights/last.pt")
-    data_yaml = r"/home/zhaotianyi/M2D-LIF/Feb_version/FLIR_data_Mul.yaml"
+    model = YOLO(r"./runs/FLIR/speed_test/weights/last.pt")
+    data_yaml = r"./data/FLIR.yaml"
     batch = 64
     epochs = 1
     device = 4
     imgsz = 640
 
-    DEFAULT_CFG.save_dir = f"../runs/v8m/val"
+    DEFAULT_CFG.save_dir = f"./runs/v8m/val"
 
     model.val(data=data, batch=batch, imgsz=imgsz, device=device, save=True)
 
